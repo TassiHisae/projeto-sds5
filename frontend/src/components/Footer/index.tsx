9ff8d8efc379863b8b23219ae74762a6e069a000
Tassiane Hisae <tassihisae@outlook.pt>
2021-11-02 14:00:55 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">Application developed by <a href="http://linkedin.com/in/tassiane-leandro-%F0%9F%87%A7%F0%9F%87%B7%F0%9F%87%B5%F0%9F%87%B9-a3a5a6176" target="_blank" rel="noreferrer">Tassiane Kitagawa</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+                    Event promoted by escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
