c5d4735771fbd66b0c738000bb406ad8e20e6b07
Tassiane Hisae <tassihisae@outlook.pt>
2021-11-04 22:13:31 -0300

    Group by search

--- backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SaleDTO.java
@@ -8,6 +8,7 @@ import java.time.LocalDate;
 public class SaleDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
     private Long id;
     private Integer visited;
     private Integer deals;
@@ -15,9 +16,7 @@ public class SaleDTO implements Serializable {
     private LocalDate date;
     private SellerDTO seller;
 
-    public SaleDTO(){
-
-    }
+    public SaleDTO(){}
 
     public SaleDTO(Long id, Integer visited, Integer deals, Double amount, LocalDate date, SellerDTO seller) {
         this.id = id;
--- backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
+++ backend/src/main/java/com/devsuperior/dsvendas/dto/SellerDTO.java
@@ -7,12 +7,11 @@ import java.io.Serializable;
 public class SellerDTO implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
     private Long id;
     private String name;
 
-    public SellerDTO(){
-
-    }
+    public SellerDTO(){}
 
     public SellerDTO(Long id, String name) {
         this.id = id;
