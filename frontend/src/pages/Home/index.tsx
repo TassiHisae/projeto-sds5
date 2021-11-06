202d0620ce4b0babab09cf163035ef65d27c5fec
Tassiane Hisae <tassihisae@outlook.pt>
2021-11-05 21:43:10 -0300

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,22 @@
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
+
 const Home = () => {
     return (
-        <div>
-            <h1>Home Page</h1>
-        </div>
+        <>
+            <NavBar />
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-4">DSVendas</h1>
+                    <p className="lead">Analyze your sales performance from different perspectives</p>
+                    <hr />
+                    <p>This app consists of displaying a panel of data provided by a backend built with Spring Boot.</p>
+                    <Link className="btn btn-primary btn-large" to="/dashboard">Dashboard Access</Link>
+                </div>
+            </div>
+            <Footer />
+        </>
     );
 }
 
